3d099a2ba377575678cc807e7b858db3a0b19203
tamim <tamimyousefi023@gmail.com>
2024-05-21 17:40:15 +0330

    add secret manager

---
+++ src/SECRETS/index.js
@@ -0,0 +1,8 @@
+export const PG_SECRETS = {
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: true,
+}
